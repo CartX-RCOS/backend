af12acdddf59d4d8afa253bf6dfbaaf59c205368
Robert Acebedo <robace747@gmail.com>
2024-01-30 09:23:55 -0500

    Implement fetchCoordinates and fetchDistance
    fetchCoordinates finds lat and long based on given address, fetchDistance finds distance between store and user location.

--- routes/getStores.js
+++ routes/getStores.js
@@ -9,9 +9,56 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const parsed = path.parse(__filename);
 
+dotenv.config();
+const apiKey = process.env.BING_KEY;
+
+// Returns an array of size 2 with the latitute and longtitude of the given address
+async function fetchCoordinates(address) {
+  let response;
+
+  // Try to make call to BingMaps API
+  try {
+    response = await fetch(`http://dev.virtualearth.net/REST/v1/Locations?addressLine=${address}&key=${apiKey}`);
+  } catch(e) {
+    console.error("Error: ", e);
+  }
+
+  if (!response.ok) {
+    throw new Error(`Network response was not ok (${response.status})`);
+  }
+
+  const data = await response.json();
+  return data.resourceSets[0].resources[0].point.coordinates;
+}
+
+
+//Uses BingMaps API to fetch the distance 
+async function fetchDistance(startCoords, endCoords) {
+  let response;
+
+  // BingMaps API calculates the distance between the user location and the given store location
+  try {
+    response = await fetch(`https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins=${startCoords}&destinations=${endCoords}&travelMode=driving&key=${apiKey}&distanceUnit=mi`)
+  } catch(e) {
+    console.error('Error:', error);
+  }
+
+  if (!response.ok) {
+    throw new Error(`Network response was not ok (${response.status})`);
+  }
+
+  const distance = await response.json();
+  
+  const dist = distance.resourceSets[0].resources[0].results[0].travelDistance;
+  const time = distance.resourceSets[0].resources[0].results[0].travelDuration;
+  return [dist, time];
+  
+}
+
 
 router.get(`/${parsed.name}`, async (req, res) => {
-  const location = "";
+  const location = "1761 15th St, Troy, NY 12180";
+  const coordinates = fetchCoordinates(location);
   
   const sampleStores = [
     'shoprite',
@@ -19,7 +66,7 @@ router.get(`/${parsed.name}`, async (req, res) => {
     'cvs'
   ];
 
-  res.json({stores: sampleStores});
+  res.json({ stores: sampleStores });
 });
 
 export default router;
