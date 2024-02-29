b026c266307174125549990c616c72b89e444e54
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2024-02-29 15:47:38 -0500

    Created sample data and began developing more sophisticated matching algorithm

---
+++ json/sampleStoreData.js
@@ -0,0 +1,105 @@
+export const sampleStoreData = {
+  "target": [
+    {
+      name: 'Red Apple',
+      price: "2",
+      quantity: "2"
+    },
+    {
+      name: 'Blue Banana',
+      price: "3",
+      quantity: "2"
+    }, 
+    {
+      name: 'Milk',
+      price: "4",
+      quantity: "5"
+    }
+  ],
+  "cvs": [
+    {
+      name: 'Red Apple',
+      price: "2.5",
+      quantity: "3"
+    },
+    {
+      name: 'Red Banana',
+      price: "3.5",
+      quantity: "2"
+    },
+    {
+      name: '2% Milk',
+      price: "3",
+      quantity: "4"
+    },
+    {
+      name: 'Skim Milk',
+      price: "4",
+      quantity: "5"
+    },
+    {
+      name: 'Oranges',
+      price: "5",
+      quantity: "5"
+    },
+    {
+      name: 'Grape',
+      price: "2",
+      quantity: "6"
+    }
+  ],
+  "hannafords": [
+    {
+      name: 'Green Apple',
+      price: "1.5",
+      quantity: "3"
+    },
+    {
+      name: 'Organic Milk',
+      price: "4.5",
+      quantity: "4"
+    },
+    {
+      name: 'Orange Juice',
+      price: "3.5",
+      quantity: "2"
+    },
+    {
+      name: 'Small Oranges',
+      price: "4.5",
+      quantity: "7"
+    }
+  ],
+  "walmart": [
+    {
+      name: "Chair",
+      price: "25",
+      quantity: "1"
+    },
+    {
+      name: "Bottled Water",
+      price: "0.99",
+      quantity: "20"
+    },
+    {
+      name: "Yellow Banana",
+      price: "2.5",
+      quantity: "10"
+    },
+    {
+      name: "1% Low Fat Milk",
+      price: "3.5",
+      quantity: "5"
+    },
+    {
+      name: "Fat Free Milk",
+      price: "4",
+      quantity: "5"
+    },
+    {
+      name: "Bright Red Apple",
+      price: "2.2",
+      quantity: "3"
+    }
+  ]
+}
--- routes/getPairedItems.js
+++ routes/getPairedItems.js
@@ -3,52 +3,71 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 const router = express.Router();
 
-import { matchItem } from '../utils/matchItem.js';
-
-
+// import { matchItem } from '../utils/matchItem.js';
+import { sampleStoreData } from '../json/sampleStoreData.js';
 
 // Get file route name (Same as file name)
 const __filename = fileURLToPath(import.meta.url);
 const parsed = path.parse(__filename);
 
-router.get(`/${parsed.name}`, async (req, res) => {
-  // console.log(getSimilarityScore("Red Apple Re", "Red Apple"))
-
-  // Example items
-  // const baseItemName = 'a red apple which I like';
-  // const items = [{name: 'red apple'}, {name: 'green apple'}, {name: 'red grape'}, {name: 'applered'}];
-  // const items = ['my apple red', 'green apple', 'red grape', 'apple red'];
-  const baseGroup = ['Red Apple', 'Blue Banana', 'Milk', '5% Milk', 'Oranges']
-  const baseStoreName = "Base Store"
-  const stores = {
-    "cvs": ['Red Apple', 'Red Banana', '2% Milk', 'brokenmilk', 'abcd', 'Grape'],
-    "hannafords": ['Apple', 'Nothing', 'Random', 'Orange juice'],
-    "walmart": ["chair", "water", "regular banana", "1% milk", "no fat milk", "apple red"]
-  }
+// Combines all items into a central format
+function createStoreMap (storeMap, stores) {
   
-  let result = {}
+  for (let store in stores) {
+    const itemMap = new Map();
+    stores[store].forEach(item => {
+      itemMap.set(item.name, item);
+    });
 
-  baseGroup.forEach(baseItem => {
-    const storeNames = Object.keys(stores)
-    result[baseItem] = {
-      "url": "https://website.com",
-      "quantity": "2",
-      "matches": {
-        [baseStoreName]: {
-          "name": baseItem,
-          "price": "$3"
-        }
-      }
-    }
-    storeNames.forEach(storeName => {
-      const storeItems = stores[storeName]
-      const match = matchItem(baseItem, storeItems)
-      result[baseItem]["matches"][storeName] = {
-        "name": match[0],
-        "price": "$2"
-      }
-    }); 
-  })
+    storeMap.set(store, itemMap)
+  }
+}
+
+// getGroups
+function getGroups (storeMap, min_matches) {
+  groups = {}
+  storeMap.forEach((items, store) => {
+    items.forEach((data, item) => {
+      console.log(data)
+    } );
+  } );
+} 
+
+router.get(`/${parsed.name}`, async (req, res) => {  
+  const storeMap = new Map();
+  createStoreMap(storeMap, sampleStoreData);
+  // console.log(Object.fromEntries(storeMap))
+
+  let result = {}
+  let minMatches = 3;
+  while (storeMap.size != 0 && minMatches > 0) {
+    const groups = getGroups(storeMap, minMatches);
+    console.log(groups)
+    minMatches--;
+  }
+
+
+  // baseGroup.forEach(baseItem => {
+  //   const storeNames = Object.keys(stores)
+  //   result[baseItem] = {
+  //     "url": "https://website.com",
+  //     "quantity": "2",
+  //     "matches": {
+  //       [baseStoreName]: {
+  //         "name": baseItem,
+  //         "price": "$3"
+  //       }
+  //     }
+  //   }
+  //   storeNames.forEach(storeName => {
+  //     const storeItems = stores[storeName]
+  //     const match = matchItem(baseItem, storeItems)
+  //     result[baseItem]["matches"][storeName] = {
+  //       "name": match[0],
+  //       "price": "$2"
+  //     }
+  //   }); 
+  // })
 
   res.json(result);
 });
