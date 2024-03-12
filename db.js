60efed4ace7a44f3e95ee1a7e64cf67c196f81c8
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2024-03-12 17:21:22 -0400

    Switched connection from SQL to mongodb as we switched databases

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
-import mysql from 'mysql2/promise';
-import 'dotenv/config';
+import { MongoClient } from 'mongodb';
 
-const pool = mysql.createPool({
-   host: process.env.DB_HOST, 
-   user: process.env.DB_USER,
-   password: process.env.DB_PASSWORD, 
-   database: process.env.DB_NAME,
-   waitForConnections: true,
-   connectionLimit: 10,
-   queueLimit: 0
-});
+const url = 'mongodb+srv://CartX:CartXRCOS@cartx.srghpey.mongodb.net/?retryWrites=true&w=majority&appName=CartX';
+const client = new MongoClient(url);
 
-export default pool;
+async function connect() {
+    try {
+        await client.connect();
+        console.log("Connected successfully to MongoDB");
+        return client.db('CartX'); 
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export const db = await connect();
