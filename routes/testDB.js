3e7faa5d43983aba811c626614110cb1c8506b0f
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2024-03-15 16:59:44 -0400

    Switched database support from sql to mongo

--- routes/testDB.js
+++ routes/testDB.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+
 const router = express.Router();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,7 +9,10 @@ const parsed = path.parse(__filename);
 
 router.get(`/${parsed.name}`, async (req, res) => {  
    try {
-      await req.db.query('SELECT 1');
+      // Assuming `req.db` is your MongoDB database instance
+      // Replace 'yourCollectionName' with an actual collection name in your database
+      // This attempts to find a single document in the specified collection
+      await req.db.collection('inventory').findOne({});
       res.json({ canConnect: true });
    } catch (err) {
       console.error('Failed to connect to the database:', err);
